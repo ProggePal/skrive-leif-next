b50cded2480adabbf95264acb15172156be9b24d
ProggePal <paal.s.peders1@gmail.com>
2025-03-06 19:31:00 +0100

    example

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,26 @@
 'use client';
 
-import { useChat } from '@ai-sdk/react';
+import { experimental_useObject as useObject } from '@ai-sdk/react';
+import { notificationSchema } from './api/use-object/schema';
 
-export default function Chat() {
-  const { messages, input, handleInputChange, handleSubmit } = useChat();
+export default function Page() {
+  const { object, submit } = useObject({
+    api: '/api/use-object',
+    schema: notificationSchema,
+  });
+
   return (
-    <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch">
-      {messages.map(m => (
-        <div key={m.id} className="whitespace-pre-wrap">
-          {m.role === 'user' ? 'User: ' : 'AI: '}
-          {m.content}
+    <div>
+      <button onClick={() => submit('Messages during finals week.')}>
+        Generate notifications
+      </button>
+
+      {object?.notifications?.map((notification, index) => (
+        <div key={index}>
+          <p>{notification?.name}</p>
+          <p>{notification?.message}</p>
         </div>
       ))}
-
-      <form onSubmit={handleSubmit}>
-        <input
-          className="fixed dark:bg-zinc-900 bottom-0 w-full max-w-md p-2 mb-8 border border-zinc-300 dark:border-zinc-800 rounded shadow-xl"
-          value={input}
-          placeholder="Say something..."
-          onChange={handleInputChange}
-        />
-      </form>
     </div>
   );
 }
